5040a8e0e43094066bf82a1a3999cb3b95c9c982
wesleyyou <yuchenxr@umich.edu>
2025-05-14 22:35:59 -0400

    feat: modify the core/hdfs-site.xml to set interval=1000ms

--- javaApiHdfs/hdfsUtils/src/main/java/hdfsUtilFuncs.java
+++ javaApiHdfs/hdfsUtils/src/main/java/hdfsUtilFuncs.java
@@ -9,19 +9,39 @@ public class hdfsUtilFuncs {
     public static void main(String[] args) throws Exception {
         // Configurations for the HDFS
         Configuration conf = new Configuration();
+        conf.set("dfs.namenode.file.close.num-committed-allowed", "1");
+        conf.setInt("dfs.blockreport.incremental.intervalMsec", 1000);
         conf.set("fs.defaultFS", "hdfs://localhost:8020");
         conf.setInt("dfs.replication", 2);
         conf.setBoolean("dfs.support.append", true);
 
         FileSystem fs = FileSystem.get(new URI("hdfs://namenode:8020"), conf);
 
-        Path remotePath = new Path("/user/youyuchen/tmp/a.txt");
+        Path remotePath = new Path("/user/youyuchen/tmp/b.txt");
         Path localPath = new Path("/workspace/java-client/src/main/java/a.txt");
         System.out.println("Uploading file to HDFS...");
 
         fs.mkdirs(new Path("/user/youyuchen/tmp"));
         System.out.println("Creating the directory /home/youyuchen/");
-        uploadFile(fs, remotePath, localPath);
+//        uploadFile(fs, remotePath, localPath);
+        bugHdfs17757(fs, remotePath, localPath);
+    }
+
+    // inject bugs hdfs-17757
+    public static void bugHdfs17757(FileSystem fs_, Path target_path_, Path local_) throws IOException {
+        FileSystem fs_local = FileSystem.getLocal(new Configuration());
+//        fs_.mkdirs(target_path_.getParent());
+        try (FSDataOutputStream out1 = fs_.create(target_path_)) {
+            out1.writeUTF("hello1\n");
+            out1.close();
+        }
+
+        try (FSDataOutputStream out2 = fs_.append(target_path_)) {
+            out2.writeUTF("hello2\n");
+            out2.close();
+        }
+
+
     }
 
     // apis to put file to hdfs
